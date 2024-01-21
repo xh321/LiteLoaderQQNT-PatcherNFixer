d7a7477a1b0cfe3b3c0d1dd7e7c632cc19e531f3
xh321 <xiaohe321@outlook.com>
2024-01-22 00:06:09 +0800

    支持手动选择QQ目录 增加不兼容提示 增加新版本修补器地址

--- FrmMain.Designer.cs
+++ FrmMain.Designer.cs
@@ -33,14 +33,20 @@
             this.lblQQDir = new System.Windows.Forms.Label();
             this.btnFixQQCorrupt = new System.Windows.Forms.Button();
             this.lblQQVersion = new System.Windows.Forms.Label();
+            this.btnRestore = new System.Windows.Forms.Button();
+            this.btnManualSelectQQDir = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnGotoNewPatcher = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnPatchQQ
             // 
             this.btnPatchQQ.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.btnPatchQQ.Location = new System.Drawing.Point(229, 88);
+            this.btnPatchQQ.Location = new System.Drawing.Point(169, 50);
             this.btnPatchQQ.Name = "btnPatchQQ";
-            this.btnPatchQQ.Size = new System.Drawing.Size(205, 62);
+            this.btnPatchQQ.Size = new System.Drawing.Size(265, 62);
             this.btnPatchQQ.TabIndex = 0;
             this.btnPatchQQ.Text = "一键修补 QQ";
             this.btnPatchQQ.UseVisualStyleBackColor = true;
@@ -50,11 +56,11 @@
             // 
             this.lblStatus.AutoSize = true;
             this.lblStatus.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.lblStatus.Location = new System.Drawing.Point(12, 425);
+            this.lblStatus.Location = new System.Drawing.Point(32, 416);
             this.lblStatus.Name = "lblStatus";
             this.lblStatus.Size = new System.Drawing.Size(109, 20);
             this.lblStatus.TabIndex = 1;
-            this.lblStatus.Text = "状态：等待";
+            this.lblStatus.Text = "状态：就绪";
             // 
             // lblQQDir
             // 
@@ -69,9 +75,9 @@
             // btnFixQQCorrupt
             // 
             this.btnFixQQCorrupt.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.btnFixQQCorrupt.Location = new System.Drawing.Point(229, 190);
+            this.btnFixQQCorrupt.Location = new System.Drawing.Point(169, 144);
             this.btnFixQQCorrupt.Name = "btnFixQQCorrupt";
-            this.btnFixQQCorrupt.Size = new System.Drawing.Size(205, 62);
+            this.btnFixQQCorrupt.Size = new System.Drawing.Size(265, 62);
             this.btnFixQQCorrupt.TabIndex = 4;
             this.btnFixQQCorrupt.Text = "一键修复 QQ 提示损坏";
             this.btnFixQQCorrupt.UseVisualStyleBackColor = true;
@@ -87,11 +93,74 @@
             this.lblQQVersion.TabIndex = 5;
             this.lblQQVersion.Text = "QQ版本：未知";
             // 
+            // btnRestore
+            // 
+            this.btnRestore.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnRestore.Location = new System.Drawing.Point(169, 239);
+            this.btnRestore.Name = "btnRestore";
+            this.btnRestore.Size = new System.Drawing.Size(265, 62);
+            this.btnRestore.TabIndex = 6;
+            this.btnRestore.Text = "恢复初始状态（还原备份）";
+            this.btnRestore.UseVisualStyleBackColor = true;
+            this.btnRestore.Click += new System.EventHandler(this.btnRestore_Click);
+            // 
+            // btnManualSelectQQDir
+            // 
+            this.btnManualSelectQQDir.Location = new System.Drawing.Point(16, 298);
+            this.btnManualSelectQQDir.Name = "btnManualSelectQQDir";
+            this.btnManualSelectQQDir.Size = new System.Drawing.Size(125, 41);
+            this.btnManualSelectQQDir.TabIndex = 7;
+            this.btnManualSelectQQDir.Text = "手动选择QQ路径";
+            this.btnManualSelectQQDir.UseVisualStyleBackColor = true;
+            this.btnManualSelectQQDir.Click += new System.EventHandler(this.btnManualSelectQQDir_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "请选择QQ.exe主程序";
+            this.openFileDialog1.Filter = "QQ.exe|QQ.exe";
+            this.openFileDialog1.InitialDirectory = "C:\\Program Files\\Tencent\\QQNT";
+            // 
+            // btnGotoNewPatcher
+            // 
+            this.btnGotoNewPatcher.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnGotoNewPatcher.ForeColor = System.Drawing.Color.Red;
+            this.btnGotoNewPatcher.Location = new System.Drawing.Point(465, 239);
+            this.btnGotoNewPatcher.Name = "btnGotoNewPatcher";
+            this.btnGotoNewPatcher.Size = new System.Drawing.Size(205, 62);
+            this.btnGotoNewPatcher.TabIndex = 8;
+            this.btnGotoNewPatcher.Text = "打开新修补器项目主页";
+            this.btnGotoNewPatcher.UseVisualStyleBackColor = true;
+            this.btnGotoNewPatcher.Click += new System.EventHandler(this.btnGotoNewPatcher_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(465, 311);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(205, 137);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "新修补器支持新版本LiteLoaderQQNT框架（版本号>1.0）且支持大于16183版本的QQNT（本修补器均不支持）";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(165, 307);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(269, 20);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "↑使用新修补器前建议使用↑";
+            // 
             // FrmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(691, 457);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnGotoNewPatcher);
+            this.Controls.Add(this.btnManualSelectQQDir);
+            this.Controls.Add(this.btnRestore);
             this.Controls.Add(this.lblQQVersion);
             this.Controls.Add(this.btnFixQQCorrupt);
             this.Controls.Add(this.lblQQDir);
@@ -115,6 +184,12 @@
         private System.Windows.Forms.Label lblQQDir;
         private System.Windows.Forms.Button btnFixQQCorrupt;
         private System.Windows.Forms.Label lblQQVersion;
+        private System.Windows.Forms.Button btnRestore;
+        private System.Windows.Forms.Button btnManualSelectQQDir;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnGotoNewPatcher;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- FrmMain.cs
+++ FrmMain.cs
@@ -9,25 +9,57 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace LiteLoaderPatchNFixer
 {
     public partial class FrmMain : Form
     {
-        private readonly string QQDir     = Utils.ReadQQDir();
-        private readonly string QQVersion = Utils.ReadQQVersoin();
+        private string QQDir       = Utils.ReadQQDir();
+        private string QQVersion   = Utils.ReadQQVersoin();
+        private bool   IsSupported = true;
         public FrmMain() { InitializeComponent(); }
 
         private void FrmMain_Load( object    sender,
                                    EventArgs e )
         {
             lblQQDir.Text = "QQ路径：" + QQDir;
-            lblQQVersion.Text = "QQ版本：" + QQVersion;
+            if ( int.Parse( QQVersion.Split( '.' )[ 3 ] ) > 16183 )
+            {
+                lblQQVersion.Text = "QQ版本：" + QQVersion + "（不受支持）";
+                lblQQVersion.ForeColor = Color.Red;
+                IsSupported = false;
+                btnPatchQQ.Enabled = false;
+                btnFixQQCorrupt.Enabled = false;
+
+                if ( MessageBox.Show( "你目前QQ版本已经高于9.9.2-16183，需要使用新版本修补器，是否打开项目主页？", "询问", MessageBoxButtons.YesNo, MessageBoxIcon.Question ) == DialogResult.Yes )
+                {
+                    Process.Start( "explorer.exe", "https://github.com/LiteLoaderQQNT/QQNTFileVerifyPatch" );
+                }
+            }
+            else
+            {
+                MessageBox.Show( "注意！如果你想支持新版本LiteLoaderQQNT框架（版本号>1.0），请使用新版本修补器！本修补器修补后仅支持旧版本框架使用。建议用过本修补器的用户进入主界面，点恢复初始状态按钮，然后再去使用新版本修补器。", "重要提示", MessageBoxButtons.OK, MessageBoxIcon.Information );
+                lblQQVersion.Text = "QQ版本：" + QQVersion;
+                IsSupported = true;
+                btnPatchQQ.Enabled = true;
+                btnFixQQCorrupt.Enabled = true;
+            }
         }
 
         private void btnPatchQQ_Click( object    sender,
                                        EventArgs e )
         {
+            if ( !IsSupported )
+            {
+                MessageBox.Show( "QQ版本不受支持，请使用新版本修补器", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information );
+                return;
+            }
+
+            MessageBox.Show( "点击确定后将终止QQ进程。", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information );
+            Process.Start( "cmd", "/c taskkill /F /IM QQ.exe" )?.WaitForExit();
+
             lblStatus.Text = "状态：备份QQ.exe";
             if ( !File.Exists( QQDir + $"\\QQ.exe.{QQVersion}.bak" ) )
             {
@@ -86,16 +118,93 @@ namespace LiteLoaderPatchNFixer
                                    .Replace( "package", "backage" ) );
 
             lblStatus.Text = "状态：完毕";
+            MessageBox.Show( "处理完毕！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information );
         }
 
         private void btnFixQQCorrupt_Click( object    sender,
                                             EventArgs e )
         {
+            if ( !IsSupported )
+            {
+                MessageBox.Show( "QQ版本不受支持，请使用新版本修补器", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information );
+                return;
+            }
+
+            MessageBox.Show( "点击确定后将终止QQ进程。", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information );
+            Process.Start( "cmd", "/c taskkill /F /IM QQ.exe" )?.WaitForExit();
+
             PatchJson();
             if ( MessageBox.Show( "尝试修复完成！是否立即打开QQ？", "提示", MessageBoxButtons.YesNo, MessageBoxIcon.Question ) == DialogResult.Yes )
             {
                 Process.Start( QQDir + "\\QQ.exe" );
             }
+        }
+
+        private void btnRestore_Click( object    sender,
+                                       EventArgs e )
+        {
+            MessageBox.Show( "注意：仅支持还原经过本修补器修补后的QQ。点击确定后自动终止所有QQ进程。", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information );
+
+            Process.Start( "cmd", "/c taskkill /F /IM QQ.exe" )?.WaitForExit();
+
+            try
+            {
+                File.Copy( QQDir + $"\\QQ.exe.{QQVersion}.bak", QQDir + "\\QQ.exe", true );
+            }
+            catch ( FileNotFoundException )
+            {
+                try
+                {
+                    File.Copy( QQDir + $"\\QQ.exe.bak", QQDir + "\\QQ.exe", true );
+                }
+                catch ( FileNotFoundException )
+                {
+                    MessageBox.Show( "还原出错！QQ.exe备份文件未找到，终止。", "提示", MessageBoxButtons.OK, MessageBoxIcon.Error );
+                    return;
+                }
+                catch ( IOException )
+                {
+                    MessageBox.Show( "还原出错！QQ.exe被占用，请先退出所有QQ进程，终止。", "提示", MessageBoxButtons.OK, MessageBoxIcon.Error );
+                    return;
+                }
+            }
+            catch ( IOException )
+            {
+                MessageBox.Show( "还原出错！QQ.exe被占用，请先退出所有QQ进程，终止。", "提示", MessageBoxButtons.OK, MessageBoxIcon.Error );
+                return;
+            }
+
+            try
+            {
+                File.Copy( QQDir + "\\resources\\app\\backage.json", QQDir + "\\resources\\app\\package.json", true );
+            }
+            catch ( FileNotFoundException ) { }
+            catch ( IOException )
+            {
+                MessageBox.Show( "还原出错！resources\\app\\package.json被占用，请先退出所有QQ进程，终止。", "提示", MessageBoxButtons.OK, MessageBoxIcon.Error );
+                return;
+            }
+
+            MessageBox.Show( "还原成功！现在QQ打开时不会加载LiteLoaderQQNT且处于未修补状态。", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information );
+        }
+
+        private void btnManualSelectQQDir_Click( object    sender,
+                                                 EventArgs e )
+        {
+            var result = openFileDialog1.ShowDialog();
+            if ( result == DialogResult.OK )
+            {
+                QQDir = new FileInfo( openFileDialog1.FileName ).DirectoryName;
+                var versionData = (JObject) JsonConvert.DeserializeObject( File.ReadAllText( QQDir + "\\resources\\app\\package.json" ) );
+                QQVersion = versionData[ "version" ].ToString().Replace( "-", "." ).Trim();
+                FrmMain_Load( null, null );
+            }
+        }
+
+        private void btnGotoNewPatcher_Click( object    sender,
+                                              EventArgs e )
+        {
+            Process.Start( "explorer.exe", "https://github.com/LiteLoaderQQNT/QQNTFileVerifyPatch" );
         }
     }
 }
